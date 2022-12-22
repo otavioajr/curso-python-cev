850a632b557a17785b4794729c7a889f7c8f4b96
Otávio Júnior <otavioajr@gmail.com>
2022-12-22 08:36:38 -0300

    Aula 10

--- PythonExercicios/ex029.py
+++ PythonExercicios/ex029.py
@@ -4,3 +4,13 @@ if vel > 80:
     print('O valor da multa é R${:.2f}'.format((vel - 80) * 7))
 else:
     print('Parabéns, você está respeitando as regras de transito!')
+
+
+### Resolução do Professor ###
+
+velocidade = float(input('Qual a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R${:.2f}'.format(multa))
+print('Tenha um bom dia! Dirija com segurança')
